72ec718f5ba535f1494359eafb23af7ad5469724
abhishekkmr82 <122913332+abhishekkmr82@users.noreply.github.com>
2024-05-14 22:45:30 +0000

    conversion of data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "abhishekgmail.com"
+var accountPassword ="12345"
+accountCity = "jaipur"
+let accountstate;
+
+//accountId = 2
+accountEmail = "asd@gmail.com"
+accountPassword ="212132"
+accountCity ="hyderabad"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountCity,accountstate]);
